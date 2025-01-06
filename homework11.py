6155dc912ecda24b0ed545bfc98fd57c469c47d9
Alexandr Vinokurov <salispiligrim@yandex.ru>
2025-01-06 15:10:38 +0500

    task 3 in homework 11 is complite

--- homework11.py
+++ homework11.py
@@ -301,14 +301,63 @@ class Lava:
             raise TypeError('Невозможно создать объект Storm напрямую. Используйте сложение Земли и Огня')
         self.name = 'Лава'
 
-water = Water()
-air = Air()
-fire = Fire()
-earth = Earth()
-storm = water + air
-steam = water + fire
-dirt = water + earth
-lightning = air + fire
-dust = earth + air
-lava = fire + earth
-print(lava.name)
+
+# Task 3
+class Rectangle:
+    def __init__(self, width, height = None):
+        self.width = width
+        if height == None:
+            self.height = width
+        else:
+            self.height = height
+
+    def area(self):
+        """
+        Method for calculating the area of a rectangle
+        :return: int
+        """
+        return self.width * self.height
+
+    def perimeter(self):
+        """
+        Method for calculating the perimeter of a rectangle
+        :return: int
+        """
+        return 2 * (self.height + self.width)
+
+    def __add__(self, other):
+        return Rectangle(self.width + other.width, self.height + other.height)
+
+    def __sub__(self, other):
+        return Rectangle(self.width - other.width, self.height - other.height)
+
+    def __lt__(self, other):
+        return self.area() < other.area()
+
+    def __eq__(self, other):
+        return self.area() == other.area()
+
+    def __le__(self, other):
+        return self.area() <= other.area()
+
+    def __str__(self):
+        return f'Прямоугольник со сторонами {self.width} и {self.height}'
+
+    def __repr__(self):
+        return f'Rectangle({self.width}, {self.height})'
+
+def task_2():
+    rect1 = Rectangle(5, 10)
+    rect2 = Rectangle(3, 7)
+    print(f"Периметр rect1: {rect1.perimeter()}")
+    print(f"Площадь rect1: {rect1.area()}")
+    print(f"Площадь rect2: {rect2.area()}")
+    print(f"rect1 < rect2: {rect1 < rect2}")
+    print(f"rect1 == rect2: {rect1 == rect2}")
+    print(f"rect1 <= rect2: {rect1 <= rect2}")
+    rect3 = rect1 + rect2
+    print(f"Периметр rect3: {rect3.perimeter()}")
+    rect4 = rect1 - rect2
+    print(f"Ширина rect4: {rect4.width}")
+
+# Task 4
